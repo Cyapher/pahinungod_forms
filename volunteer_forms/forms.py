cc60d8968edce5ee14a724199ff3e4b64cf51f4e
maikerucode <michaeljoshuasarmiento@gmail.com>
2024-03-06 17:04:50 +0800

    form edits

--- volunteer_forms/forms.py
+++ volunteer_forms/forms.py
@@ -7,3 +7,17 @@ class VolunteerForm(ModelForm):
     class Meta:
         model = Volunteer
         fields = '__all__'
+
+    placeholders = {
+        'first_name' : 'Enter your first name here'
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(VolunteerForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, forms.CharField):
+                field.widget.attrs.update({
+                    'class': 'form-control'
+                })
+            if field_name in self.placeholders:
+                field.widget.attrs['placeholder'] = self.placeholders[field_name]
