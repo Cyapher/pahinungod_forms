133e23eb3de6d18dad3962684001235093c31157
maikerucode <65951580+maikerucode@users.noreply.github.com>
2024-04-30 16:17:57 +0800

    minor changes

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 from django.contrib.auth.decorators import login_required, user_passes_test
 
 def is_superuser(user):
-    return user.is_authenticated and user.is_superuser
+    return user.is_authenticated and (user.is_superuser or user.is_staff)
 
 @login_required(login_url='home_vol')
 @user_passes_test(is_superuser, login_url='home_vol')
