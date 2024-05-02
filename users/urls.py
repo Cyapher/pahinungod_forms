e5cb50893f23c26950cb73d40519d2081f5c80c9
maikerucode <65951580+maikerucode@users.noreply.github.com>
2024-05-02 15:13:32 +0800

    users removed

--- users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from . import views
-
-urlpatterns = [
-    path('login/', views.login, name='login'),
-]
